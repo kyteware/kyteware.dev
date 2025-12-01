773a75bc964e4e341a329306ab6a8be2d82a2b54
Kyteware <kyteware.biz@gmail.com>
2025-11-30 19:21:29 -0800

    feat: move  to a canvas

--- exploder/src/main.rs
+++ exploder/src/main.rs
@@ -4,7 +4,13 @@ use exploder::{dropping_plugin, loader_plugin, waiting_plugin, VisState};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                canvas: Some("#gumball-canvas".into()),
+                ..default()
+            }),
+            ..default()
+        }))
         // .add_plugins(EguiPlugin::default())
         // .add_plugins(WorldInspectorPlugin::new())
         .add_plugins(PhysicsPlugins::default())
