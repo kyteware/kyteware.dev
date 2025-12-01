d4e5dc15eb1360d5cdb681235a6fb675577aeb35
Kyteware <kyteware.biz@gmail.com>
2025-11-30 19:35:26 -0800

    ...

--- gumballs/build-for-web.sh
+++
@@ -1,3 +0,0 @@
-cargo build --release --target wasm32-unknown-unknown
-wasm-bindgen --no-typescript --target web --out-dir ./web_resource_out/ --out-name "gumball" ./target/wasm32-unknown-unknown/release/exploder.wasm
-python -m http.server --directory web_resource_out
